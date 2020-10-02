f0c0f04b9119ae55e7fcf40d6e937a312742f918
1670995411@qq.com <1670995411@qq.com>
2020-10-02 08:39:38 +0800

    💎 Jewelry and stones

---
+++ 字符串/宝石和石头.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+ * 即在字符串S中找寻字符串J出现的所有字母
+ */
+class Solution {
+public:
+    int numJewelsInStones(string J, string S) {
+        int ans = 0;
+        unordered_map<char, bool> m;
+        for(const char &i : J) m[i] = true;
+        for(const char &i : S)
+        {
+            if(m[i]) ++ans;
+        }
+        return ans;
+    }
+};
