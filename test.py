0988814e74c925e2b6293a60903e1629923994a2
1670995411@qq.com <1670995411@qq.com>
2021-01-13 12:08:52 +0800

    Regularization Match

--- test.py
+++ test.py
@@ -1,16 +1,61 @@
-class Solution:
-    def concatenatedBinary(self, n: int) -> int:
-        res = []
-        for i in range(n, 0, -1):
-            j = i
-            while j :
-                res.append('1' if j & 1 else '0')
-                j >>= 1
-        res = res[::-1]
-        ans = 0
-        r = 0
-        for i in range(len(res) - 1, -1, -1):
-            if res[i] is '1':
-                ans += 1 * pow(2, r)
-            r = r + 1
-        return ans % (1000000000 + 7)
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+
+        dic = {}
+        state = 1
+        for idx, i in enumerate(p):
+            if 'a' <= i <= "z" or i == '.':
+                dic.setdefault(state, {}).update({i: state + 1})
+                state += 1
+            elif i == '*':
+                dic[state - 1][p[idx - 1]] = state - 1
+                dic[state - 1]['$'] = state
+
+        def helper(sta, px):
+            if px:
+                try:
+                    tmp1 = dic[sta].get(px[0], False)
+                    tmp2 = dic[sta].get('.', False)
+                    tmp3 = dic[sta].get('$', False)
+                except:
+                    return False
+                if tmp1:
+                    if helper(tmp1, px[1:]):
+                        return True
+                if tmp2:
+                    if helper(tmp2, px[1:]):
+                        return True
+                if tmp3:
+                    if helper(tmp3,px):
+                        return True
+                return False
+            else:
+                try:
+                    tmp = dic[sta].get('$', False)
+                    return helper(tmp, px)
+                except:
+                    pass
+                if sta == state:
+                    return True
+                return False
+        return helper(1, s)
+
+
+s = Solution()
+tests = [
+    ("babbcaacbccbbbbbabb","bb*.b*b*a*aba*c"),
+    ("aa","a*"),
+    ("abbabaaaaaaacaa", "a*.*b.a.*c*b*a*c*"),
+    ("aaba", "ab*a*c*a"),
+    ("aaa", "a*a"),
+    ("abcdede", "ab.*de"),
+]
+
+for item in tests:
+    res = s.isMatch(*item)
+    print(res)
--- 剑指offer/19-正则表达式.cc
+++ 剑指offer/19-正则表达式.cc
@@ -26,19 +26,67 @@ public:
 //     a        b  \   /  $         c
 // 0 -----> 1 -----> 2  -----> 3 -----> 4
 
-// 构建完成后再通过循环或者递归的方式对s进行判断
+// 构建完成后再通过dfs的方式对s进行判断
+// 当然，dfs的方法很慢，C++代码会通过全部测试用例然后超时...（就很迷，明明都通过了）
 
 class Solution {
-    // 由于该题中一个状态下不会转移到多个状态
-    // 所以直接用vector下标表示，而不用map
-    // 下标为当前状态，char为输出，int为下一状态
-    vector<pair<char, int>> table;
+    // key为当前状态，char为下一字符，int为转移到的状态
+    unordered_map<int, unordered_map<char, int>> table;
+    int end_state;
 public:
-    void construct_dfa(const string& s) {
+    void constructDFA(const string& p) {
+        int state = 0;
+        for (int i = 0; i < p.size(); ++i) {
+            char c = p[i];
+            if (c >= 'a' && c <= 'z' || c == '.') {
+                table[state].clear();
+                table[state][c] = state + 1;
+                ++state;
+            }
+            if (c == '*') {
+                table[state - 1][p[i - 1]] = state - 1; // 转移到自己，会覆盖旧值
+                table[state - 1]['$'] = state; // 输入为空转移到下一状态，index不自增
+            } 
+        }
+        end_state = state;
+        // for (const auto& [fir, sub] : table) {
+        //     for (const auto& ss : sub) {
+        //         cout << fir << " " << ss.first << " " << ss.second << endl;
+        //     }
+        // }
+    }
 
+    // cur代表当前状态，i为当前字符
+    bool dfs(int cur, const string&s, int i) {
+        if(i >= s.size() && cur == end_state) return true;
+
+        // 注意三者匹配只要有一个符合就返回true
+        if (i < s.size()) {
+            if (table[cur].find(s[i]) != table[cur].end()) { // 当前字符
+                int cur1 = table[cur][s[i]];
+                if(dfs(cur1, s, i + 1)) return true;
+            }
+            if (table[cur].find('.') != table[cur].end()) { // '.'
+                int cur2 = table[cur]['.'];
+                if(dfs(cur2, s, i + 1)) return true;
+            }
+            if (table[cur].find('$') != table[cur].end()) { // '$'
+                int cur3 = table[cur]['$'];
+                if(dfs(cur3, s, i))     return true; // 注意是空字符这里下标i不变
+            }
+            return false;
+        }
+        else { // 如果已经到了字符串末尾，需要判断可以一直为空走到底的情况
+            if (table[cur].find('$') != table[cur].end()) { // '$'
+                cur = table[cur]['$'];
+                return dfs(cur, s, i); // 注意是空字符这里下标i不变
+            }
+        }
+        return false;
     }
 
     bool isMatch(string s, string p) {
-        
+        constructDFA(p);
+        return dfs(0, s, 0);
     }
 };
