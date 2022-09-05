ae06f779052732dee694c22926e7b6a2fa0e33a2
UnendingGlory <1670995411@qq.com>
2022-09-05 21:45:38 +0800

    three sum

--- [1] 合集/hot100/72.编辑距离.py
+++ [1] 合集/hot100/72.编辑距离.py
@@ -6,20 +6,27 @@
 # 思路：dp[i][j] 代表串 s1[0...i] 和 s2[0...j] 之间的编辑距离。
 # 定理 1：如果一个串是空串，那么编辑距离即另一个串的长度。
 # 定理 2：对于 dp[i][j]，如串 s1 "horse", 串 s2 "ros"，有三种情况。
-#     ① 串 "hors" 和 串 "ros" 之间编辑距离 + 1，即 dp[i][j] = dp[i-1][j] + 1
-#     ② 串 "horse" 和串 "ro" 之间编辑距离 + 1，即 dp[i][j] = dp[i][j-1] + 1
-#     ③ 串 "hors" 和 串 "ro" 之间编辑距离 + int(最后一个字符是否需要替换)
-#        即 dp[i][j] = dp[i-1][j-1] + int(s1[i] == s2[j])
+#     注意，对串 s1操作和对串 s2操作是等价的，我们这里只考虑对串 s1操作。
+#     1. 如果 s1[i] == s2[j]，那么我们可以将 s1[i] 和 s2[j]一起去掉，dp[i][j] = dp[i - 1][j - 1]
+#     2. 如果 s1[i] != s2[j]，那么我们考虑操作 s1，按照题意，有三种操作方式。
+#     ① 在串 "horse"后插入 's'，那么 "horses" 和 "ros"抵消称为 "horse" 和 "ro"。（将新插入的尾部字符抵消）
+#        即 dp[i][j] = dp[i][j-1] + 1（插入操作）  
+#     ② 在串 "horse"尾部执行删除操作，就是 "hors" 和 "ros"。
+#        即 dp[i][j] = dp[i-1][j] + 1（删除操作）  
+#     ③ 在串 "horse" 和 串 "ros"尾部执行替换操作，替换成 "horss" 和 "ros"。
+#        即 dp[i][j] = dp[i-1][j-1] + 1 （替换操作）  
 # 转移方程：dp[i][j] = min(dp[i-1][j] + 1, dp[i][j-1] + 1, 
-#                        dp[i-1][j-1] + int(s1[i] != s2[j]))
+#                        dp[i-1][j-1] + int(s1[i - 1] != s2[j - 1]))
 # 边界条件：dp[0][j] = j, dp[i][0] = i。（对应定理1）
+# 返回答案：dp[m][n]
 # 时间复杂度：O(mn)，空间复杂度：O(mn)
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         m, n = len(word1), len(word2)
         if m == 0 or n == 0:
             return m | n
-        dp = [[101] * (n+1) for _ in range(m+1)] # 最大编辑距离，应该设为INT_MAX
+        # 数组每一维多开辟一格留给空串，最大编辑距离，应该设为INT_MAX
+        dp = [[101] * (n+1) for _ in range(m+1)] 
 
         for i in range(m+1): # 0 代表空串
             dp[i][0] = i
@@ -28,7 +35,7 @@ class Solution:
         for i in range(1, m+1):
             for j in range(1, n+1):
                 dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + 1
-                # 这里是 i-1 和 j-1，因为 dp数组顾及空串，比原串大
+                # 注意下标，这里是 i-1 和 j-1，因为 dp数组顾及空串，比原串大
                 dp[i][j] = min(dp[i][j], dp[i-1][j-1] + int(word1[i-1] != word2[j-1]))
         return dp[-1][-1] # dp[m][n]
 
---
+++ [1] 合集/面试散手刷/997.找到小镇的法官.cpp
@@ -0,0 +1,28 @@
+#include "header.h"
+
+小镇里有 n 个人，按从 1 到 n 的顺序编号。传言称，这些人中有一个暗地里是小镇法官。
+如果小镇法官真的存在，那么：
+小镇法官不会信任任何人。
+每个人（除了小镇法官）都信任这位小镇法官。
+只有一个人同时满足属性 1 和属性 2 。
+给你一个数组 trust ，其中 trust[i] = [ai, bi] 表示编号为 ai 的人信任编号为 bi 的人。
+如果小镇法官存在并且可以确定他的身份，请返回该法官的编号；否则，返回 -1 。
+
+// Solution：图的入度出度概念。
+// 按照题意，只有法官的入度为 n - 1，出度为 0。
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> indegree(n + 1), outdegree(n + 1);
+        for (auto &p: trust) {
+            outdegree[p[0]]++;
+            indegree[p[1]]++;
+        }
+        for (int i = 1; i <= n; ++i) {
+            if (indegree[i] == n - 1 && outdegree[i] == 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
